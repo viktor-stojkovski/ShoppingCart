efea850a9af586014c2cecfc0a3eb936600b5290
Viktor Stojkovski <viktor.stojkovski87@gmail.com>
2026-03-30 12:43:32 +0200

    initial commit: shopping cart solution and empty web api created

---
+++ ShoppingCart/Controllers/ShoppingCartController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingCart.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShoppingCartController : ControllerBase
+    {
+        private readonly ILogger<ShoppingCartController> _logger;
+
+        public ShoppingCartController(ILogger<ShoppingCartController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
